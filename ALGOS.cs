d0cee00e8c992ed87b581310fc5f1f652e91466d
tager1199 <32684792+tager1199@users.noreply.github.com>
2018-04-17 18:17:40 +0100

    Bubble Sort v2.0
    Bubble sort working with a time complexity counter

--- ALGOS.cs
+++ ALGOS.cs
@@ -13,7 +13,10 @@ namespace ConsoleApplication1
     {
       string Data = DataSelection();
       double[] Unsorted = LoadData(Data);
-
+      double[] Sorted = BubbleSort(Unsorted);
+      foreach(double i in Sorted){
+        Console.WriteLine(i);
+      }
     }
 
     static string DataSelection()
@@ -76,33 +79,20 @@ namespace ConsoleApplication1
 
       static double[] BubbleSort(double[] Unsorted)
         {
-
-          Len = Unsorted.Length;
-          double[] Sorted = new double[Unsorted.Length];
           double temp = 0;
-
-
-          for (int i = 0; i < Len-1; i++){
-            bool swapped = false;
-
-            for (int j = 0; j < Len-1; i++){
-                /* compare the adjacent elements */
-                if (list[j] > list[j+1]) {
-                  /* swap them */
-                  swapped = true;
-                  temp = list[j];
-                  list[j] = list[j+1];
-                  list[j+1] = temp;
-                }
-               /*if no number was swapped that means
-               array is sorted now, break the loop.*/
-
-               if(swapped == false){
-                 break
-                 }
-               }
-            }
-            return Sorted;
+          int TimeCount = 0;
+          for (int i = 0; i < Unsorted.Length; i++) {
+              for (int j = 0; j < Unsorted.Length - 1; j++) {
+                  TimeCount+=1;
+                  if (Unsorted[j] > Unsorted[j + 1]) {
+                      temp = Unsorted[j + 1];
+                      Unsorted[j + 1] = Unsorted[j];
+                      Unsorted[j] = temp;
+                  }
+              }
+          }
+            Console.WriteLine("The time complexity for this algorim is {0}",TimeCount);
+            return Unsorted;
         }
 
   }
